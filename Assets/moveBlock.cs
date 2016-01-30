bd7125e26bb3352150d021d752e1100b8aec5f71
Mattis Lefalhun <Mattis Lefalhun>
2016-01-30 01:09:09 +0100

    mattisdelamerde

---
+++ Assets/moveBlock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBlock : MonoBehaviour {
+
+    public int nbLine = 14;
+    public int nbColone = 10;
+    public float moveSpeed = 0.4f;
+
+    public GameObject block;
+    public GameObject blocke;
+    public GameObject blockm;
+        
+     
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+	}
+}
